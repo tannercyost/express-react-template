4a474b79ec4bfda62df75f96aaa06b4520706b09
Tanner Yost <Tanner.Yost@sonoma-county.org>
2024-01-30 14:43:05 -0800

    Rearranged some names

--- client/src/components/thing.tsx
+++ client/src/components/thing.tsx
@@ -9,16 +9,17 @@ type Item = {
   completed : boolean
 }
 
-function Items(props: { items: Item }) {
+function Thing(props: { items: Item, count: number }) {
   // const [count, setCount] = useState(0)
   // const [items, setItems] = useState<Item[]>([]);
   const item = props.items
-
+  const count = props.count
   return (
     <> 
+      <h1>Current count from above: {count}</h1>
       <p key={item.id}>{item.id} : {item.description}</p>
     </>
   )
 }
 
-export default Items
+export default Thing
